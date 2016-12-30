aeb058f1449489d8e66aa11add2db3a7bb867de4
Brandon Elder <brandonelder3@gmail.com>
2016-12-30 20:51:20 +0000

    section & lesson modals

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Flixter::Application.routes.draw do
 
   devise_for :users
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
@@ -10,7 +11,7 @@ Flixter::Application.routes.draw do
     resources :sections, only: [:update]
     resources :lessons, only: [:update]
     resources :sections, only: [] do
-      resources :lessons, only: [:create, :new]
+      resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create, :new]
